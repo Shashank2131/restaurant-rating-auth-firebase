066ccbba9892552fa72df142774dd1f59d70d55f
Shashank Ravindranath <85211196+Shashank2131@users.noreply.github.com>
2021-06-16 09:43:31 +0530

    Add files via upload

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,1 +1,46 @@
+import React, { useState, useEffect } from 'react'
+import './styles.css'
+import HomePage from '../HomePage'
+import Login from '../Login'
+import Register from '../Register'
+import Dashboard from '../Dashboard'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+import { CssBaseline, CircularProgress } from '@material-ui/core'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import firebase from '../firebase'
+import Logout from "../Dashboard"
+import List from '../List'
 
+
+
+const theme = createMuiTheme()
+
+export default function App() {
+	
+	const [firebaseInitialized, setFirebaseInitialized] = useState(false)
+
+	useEffect(() => {
+		firebase.isInitialized().then(val => {
+			setFirebaseInitialized()
+		})
+	})
+	
+			
+	
+
+	return firebaseInitialized !== false ? (
+		<MuiThemeProvider theme={theme}>
+			<CssBaseline />
+			<Router>
+				<Switch>
+					<Route exact path="/" component={HomePage} />
+					<Route exact path="/login" component={Login} />
+					<Route exact path="/register" component={Register} />
+					<Route exact path="/dashboard" component={Dashboard} />
+					<Route exact path="/list" component={List} />
+					<Route exact path="/" component={Logout} />
+				</Switch>
+			</Router>
+		</MuiThemeProvider>
+	) : <div id="loader"><CircularProgress /></div>
+}
